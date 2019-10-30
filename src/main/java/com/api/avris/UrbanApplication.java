c552bdfa0e2fc7df8b2096ed756e675f12787b7d
afris <afrisilmannafian@gmail.com>
2019-10-30 13:25:42 +0700

    initial commit

---
+++ src/main/java/com/api/avris/UrbanApplication.java
@@ -0,0 +1,13 @@
+package com.api.avris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanApplication.class, args);
+	}
+
+}
